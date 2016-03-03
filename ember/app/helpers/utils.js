aa37824f3eb852b25cbbde859d413eaaee34c62b
Patrick Fox <patrick.fox@razorfish.com>
2016-03-03 16:44:41 -0600

    Added A2 example

--- ember/app/helpers/utils.js
+++ ember/app/helpers/utils.js
@@ -5,7 +5,7 @@ const site_title = 'Modern Accessibility with Ember - ';
 function announce_view_loaded() {
 	var page_title, page_title_el;
 	page_title_el = document.querySelector('[data-page-title]');
-	if (page_title_el != null) {
+	if (page_title_el !== null) {
 		page_title = page_title_el.getAttribute('data-page-title') || page_title_el.innerText;
 		page_title = page_title === 'true'? page_title_el.innerText : page_title;
 	} else {
@@ -26,17 +26,21 @@ function announce(message, manners) {
 	var announcer, clear_announcer;
 	manners = manners || 'polite';
 	announcer = document.getElementById('a11y_announcer');
-	announcer.setAttribute('aria-live', 'off');
-	clear_announcer = function() {
-		announcer.innerHTML = '';
-		announce_timeout = null;
+	if (announcer) {
+		announcer.setAttribute('aria-live', 'off');
+		clear_announcer = function() {
+			announcer.innerHTML = '';
+			announce_timeout = null;
+			return announcer;
+		};
+		announcer.setAttribute('aria-live', manners);
+		announcer.innerHTML = message;
+		clearTimeout(announce_timeout);
+		announce_timeout = setTimeout(clear_announcer, 500);
 		return announcer;
-	};
-	announcer.setAttribute('aria-live', manners);
-	announcer.innerHTML = message;
-	clearTimeout(announce_timeout);
-	announce_timeout = setTimeout(clear_announcer, 500);
-	return announcer;
+	} else {
+		alert('a11y_announcer not found');
+	}
 };
 
 export { announce_view_loaded }
--- ember/dist/assets/library-app.js
+++ ember/dist/assets/library-app.js
@@ -4312,7 +4312,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("library-app/app")["default"].create({"name":"library-app","version":"2.3.0+15cc4ba4"});
+  require("library-app/app")["default"].create({"name":"library-app","version":"2.3.0+57f3f8f3"});
 }
 
 /* jshint ignore:end */
